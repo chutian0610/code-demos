98c3c5bf665474744f6c7f473fca8c20573a93b8
chutian <chutian0610@users.noreply.github.com>
2020-06-30 10:12:34 +0800

    fix: mybatis sql

--- mybatis/mybatis/sql/env.sql
+++ mybatis/mybatis/sql/env.sql
@@ -1,4 +1,4 @@
-create user 'demo'@'localhost' identified BY '1qazXSW@';
+create user 'demo'@'%' identified BY '1qazXSW@';
 create database tests character set utf8;
-grant all privileges on tests.* to 'demo'@'localhost';--
+grant all privileges on tests.* to 'demo'@'%';
 flush privileges;
